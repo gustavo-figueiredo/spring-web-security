e16575cbee589dfca9303ea2061f0792bd1e2edf
Gustavo Figueiredo <gf1985@gmail.com>
2024-08-22 12:00:39 -0300

    first commit

---
+++ src/main/java/com/example/spring_web_security/SpringWebSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSecurityApplication.class, args);
+	}
+
+}
