8d1233b982c89592fc5e0e177f69a9b6a24a5949
Gustavo Figueiredo <gf1985@gmail.com>
2024-08-25 09:45:03 -0300

    remove InMemoryUserDetailsManager, move formLogin to httpBasic

--- src/main/java/com/example/spring_web_security/config/WebSecurityConfig.java
+++ src/main/java/com/example/spring_web_security/config/WebSecurityConfig.java
@@ -1,45 +1,38 @@
 package com.example.spring_web_security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
 
-    @Bean
-    public UserDetailsService users() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("{noop}12345")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}12345")
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+        @Autowired
+        private SecurityDatabaseService securityService;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
-                                .requestMatchers("/").permitAll()
-                                .requestMatchers("/login").permitAll()
-                                .requestMatchers("/admins").hasAnyRole("ADMIN")
-                                .requestMatchers("/users").hasAnyRole("USER", "ADMIN")
-                                .anyRequest().authenticated()).formLogin(Customizer.withDefaults());
-        return http.build();
-    }
+        @Autowired
+        public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+        }
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http.csrf(AbstractHttpConfigurer::disable)
+                                .authorizeHttpRequests(
+                                                authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                                                                .requestMatchers("/").permitAll()
+                                                                .requestMatchers("/login").permitAll()
+                                                                .requestMatchers("/admins").hasAnyRole("ADMIN")
+                                                                .requestMatchers("/users").hasAnyRole("USER", "ADMIN")
+                                                                .anyRequest().authenticated())
+                                .httpBasic(Customizer.withDefaults());
+                return http.build();
+        }
 
 }
