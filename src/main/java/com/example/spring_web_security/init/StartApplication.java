1781742d3da4544a225adfe57252ca9d4079c446
Gustavo Figueiredo <gf1985@gmail.com>
2024-08-25 09:47:29 -0300

    JPA Authentication

---
+++ src/main/java/com/example/spring_web_security/init/StartApplication.java
@@ -0,0 +1,36 @@
+package com.example.spring_web_security.init;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.spring_web_security.model.User;
+import com.example.spring_web_security.repositories.UserRepository;
+
+@Component
+public class StartApplication implements CommandLineRunner {
+    @Autowired
+    private UserRepository repository;
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+        User user = repository.findByUsername("admin");
+        if(user==null){
+            user = new User();
+            user.setName("ADMIN");
+            user.setUsername("admin");
+            user.setPassword("admin123");
+            user.getRoles().add("ADMIN");
+            repository.save(user);
+        }
+        user = repository.findByUsername("user");
+        if(user ==null){
+            user = new User();
+            user.setName("USER");
+            user.setUsername("user");
+            user.setPassword("user123");
+            user.getRoles().add("USER");
+            repository.save(user);
+        }
+    }
+}
---
+++ src/main/java/com/example/spring_web_security/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.spring_web_security.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.spring_web_security.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+}
